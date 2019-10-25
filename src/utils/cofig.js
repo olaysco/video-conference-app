4825182d5c18aacd71a83443f12a7d61565f9253
olaysco <olayiwolaodunsi@gmail.com>
2019-10-25 19:06:56 +0100

    change server url

--- src/utils/cofig.js
+++ src/utils/cofig.js
@@ -1,8 +1,7 @@
 export const url = `${process.env.VUE_APP_SOCKET_HOST || "localhost"}:${process
   .env.VUE_APP_SOCKET_PORT || "3000"}`;
 
-export const SERVER_URL = `${process.env.VUE_APP_SOCKET_HOST ||
-  "localhost"}:${process.env.VUE_APP_SOCKET_PORT || "3050"}`;
+export const SERVER_URL = "https://conf-chat.herokuapp.com";
 
 export const STORE_ACTIONS = {
   joinRoom: "joinRoom",
